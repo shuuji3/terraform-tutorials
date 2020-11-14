0b83c49737478e1b7afbd1d6d07c53d4a54ff844
TAKAHASHI Shuuji <shuuji3@gmail.com>
2020-11-14 02:04:23 -0500

    Add network tags.

--- main.tf
+++ main.tf
@@ -23,6 +23,10 @@ resource "google_compute_network" "vpc_network" {
 resource "google_compute_instance" "vm_instance" {
   name = "terraform-instance"
   machine_type = "f1-micro"
+  tags = [
+    "web",
+    "dev",
+  ]
 
   boot_disk {
     initialize_params {
